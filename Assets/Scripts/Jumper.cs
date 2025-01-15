b6e2b6a252246509d7f4effdf1f72f964c2dcdce
JK <piromaniak@gmail.com>
2025-01-15 20:03:13 +0100

    Added sound to BellBasic

--- Assets/Scripts/Jumper.cs
+++ Assets/Scripts/Jumper.cs
@@ -5,14 +5,22 @@ public class Jumper : MonoBehaviour
     private float jumpForce; // Jump power.
     private int addedPoints; // Jump power.
     PlayerPointsObject points;
-    [SerializeField]
-    bool extraJumper = false;
+    [SerializeField] bool extraJumper = false;
+
+    [Header("Ball Collision Sound Settings")]
+    [Tooltip("The sound played when the ball hits something.")]
+    [SerializeField] private AudioClip hitSound;
+
+    private AudioSource audioSource;
+
     private void Awake()
     {
         jumpForce = extraJumper ? 0.01f : 0.015f;
         addedPoints = extraJumper ? 1 : 2;
         points = FindObjectOfType<PlayerPointsObject>();
+        audioSource = GetComponent<AudioSource>();
     }
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ball"))
@@ -20,6 +28,8 @@ public class Jumper : MonoBehaviour
             Rigidbody ballRigidbody = collision.gameObject.GetComponent<Rigidbody>();
             if (ballRigidbody != null)
             {
+                audioSource.PlayOneShot(hitSound);
+
                 // Adding Jump Force
                 ballRigidbody.AddForce(Vector3.up * jumpForce);
 

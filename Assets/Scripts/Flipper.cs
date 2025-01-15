1d15344d59a619684ec93649474291b4baff1019
JK <piromaniak@gmail.com>
2025-01-15 13:29:14 +0100

    Flippers - bounce physics -bounce angle based on flipper normal vector -added angle modifier to make flippers bounce ball more towards center of the board -velocity is now proportional to the speed of the flipper -added hit strength independend of spring strenght -clamping max ball speed -tweaked flippers variable values

--- Assets/Scripts/Flipper.cs
+++ Assets/Scripts/Flipper.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class Flipper : MonoBehaviour
 {
     [SerializeField] float restPos = 0f;
     [SerializeField] float pressedPos = 45f;
-    [SerializeField] float hitStrenght = 1000f;
+    [SerializeField] float springStrenght = 7000f;
+    [SerializeField] float hitStrenght = 5f;
     [SerializeField] float flipperDamper = 50f;
+    [SerializeField] float maxSpeed = 100f;
     [SerializeField] private bool leftFlipper;
+    float curveModifierLeft = 0.5f;
+    float curveModifierRight = 0.5f;
     private HingeJoint hinge;
     private JointSpring spring;
 
+    // Flippers control keys
+    [SerializeField] private KeyCode[] leftKeys = { KeyCode.A, KeyCode.LeftArrow };
+    [SerializeField] private KeyCode[] rightKeys = { KeyCode.D, KeyCode.RightArrow };
 
     void Start()
     {
@@ -20,6 +28,7 @@ public class Flipper : MonoBehaviour
         spring = new JointSpring();
         hinge.useLimits = true;
     }
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ball"))
@@ -27,27 +36,60 @@ public class Flipper : MonoBehaviour
             Rigidbody ballRigidbody = collision.gameObject.GetComponent<Rigidbody>();
             if (ballRigidbody != null)
             {
-                // Additional impulse in the direction of movement of the flipper
+                Vector3 ballVelocity = ballRigidbody.velocity;
+                Vector3 collisionNormal = collision.contacts[0].normal;
+                //Arcade
+                if (leftFlipper)
+                {
+                    collisionNormal = Vector3.Lerp(collisionNormal, transform.right, curveModifierLeft);
+                }
+                else
+                {
+                    collisionNormal = Vector3.Lerp(collisionNormal, -transform.right, curveModifierRight);
+                }
 
-                Vector3 flipperForce = transform.right * hitStrenght * 0.001f; // Direction and strength of impulse
-                ballRigidbody.AddForce(flipperForce, ForceMode.Impulse);
+                Vector3 reflectedVelocity = Vector3.Reflect(ballVelocity, collisionNormal);
+
+                Vector3 flipperVelocity = Vector3.zero;
+                Rigidbody flipperRigidbody = GetComponent<Rigidbody>();
+                if (flipperRigidbody != null)
+                {
+                    flipperVelocity = flipperRigidbody.velocity;
+                }
+
+                Vector3 finalVelocity = reflectedVelocity + flipperVelocity * hitStrenght;
+                //Arcade
+                finalVelocity = Vector3.ClampMagnitude(finalVelocity, maxSpeed);
+
+
+                ballRigidbody.AddForce(finalVelocity - ballRigidbody.velocity, ForceMode.VelocityChange);
             }
-            Debug.Log("Flipper hit the ball with velocity: " + collision.relativeVelocity.magnitude);
         }
     }
 
+    private bool IsAnyKeyPressed(KeyCode[] keys)
+    {
+        foreach (KeyCode key in keys)
+        {
+            if (Input.GetKey(key))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 
     void Update()
     {
-        spring.spring = hitStrenght;
+        spring.spring = springStrenght;
         spring.damper = flipperDamper;
+        hinge.spring = spring;
 
-
-        if (leftFlipper && Input.GetAxis("Horizontal") < 0)
+        if (leftFlipper && IsAnyKeyPressed(leftKeys))
         {
             spring.targetPosition = pressedPos;
         }
-        else if (!leftFlipper && Input.GetAxis("Horizontal") > 0)
+        else if (!leftFlipper && IsAnyKeyPressed(rightKeys))
         {
             spring.targetPosition = pressedPos;
         }
@@ -55,7 +97,5 @@ public class Flipper : MonoBehaviour
         {
             spring.targetPosition = restPos;
         }
-
-        hinge.spring = spring;
     }
 }
